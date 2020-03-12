b52c326ab6884c6923d57d5f412af2f32fe8f38d
kimhjj <kimhj@gaia3d.com>
2020-03-12 20:37:03 +0900

    정리

--- src/main/resources/static/js/gis.js
+++ src/main/resources/static/js/gis.js
@@ -69,6 +69,106 @@ var MapInit = function(policy) {
         transactWFS('insert', feature);
     });
 
+    var formatWFS = new ol.format.WFS();
+    var formatGML = new ol.format.GML({
+    	featureNS: 'pokemap',
+    	featureType: 'pokestop_info',
+    	version: '1.1.0'
+    });
+
+    var transactWFS = function(action, feature) {
+    	if(feature == null)
+    		return;
+
+
+    	var node;
+    	switch (action) {
+        case 'insert':
+        	feature.set('code', 'STOP');
+        	feature.set('name', 'Insert Test test test');
+        	node = formatWFS.writeTransaction([feature], null, null, formatGML);
+        	break;
+
+        case 'update':
+        	feature.set('code', 'STOP');
+        	feature.set('name', 'Update Update Update Update');
+        	node = formatWFS.writeTransaction(null, [feature], null, formatGML);
+        	break;
+
+        	case 'delete':
+        	node = formatWFS.writeTransaction(null, null, [feature], formatGML);
+        	break;
+    	}
+
+    	$.ajax({
+    	    service: 'WFS',
+    	    type: "POST",
+    	    url: "http://localhost:8080/geoserver/pokemap/wfs",
+    	    dataType: 'xml',
+    	    processData: false,
+    	    contentType: 'text/xml',
+    	    data: new XMLSerializer().serializeToString(node),
+    	    contentType: 'text/xml',
+    	    success: function(data) {
+    	    	debugger
+    	    	console.log(formatWFS.readTransactionResponse(data));
+    	    },
+    	    error: function(e) {
+    	    	console.error(e);
+    	    },
+    	    context: this
+//    	}).done(function() {
+//    	    vectorSource.clear();
+//    	    map.removeInteraction(draw);
+//    	    map.removeInteraction(select);
+//    	    select.getFeatures().clear();
+    	});
+    };
+
+    /**
+     * 파라미터로 받은 좌표로 Feature를 생성하여 리턴한다.
+     * @param   {string} geomType (Point, Polygon)
+     * @param   {Array<string>} coord
+     * @returns {Object} ol.Feature
+     */
+    var getFeatureByCoord = function(geomType, coord) {
+        // validation
+
+        var shape = {
+            'Point': new ol.geom.Point(coord),
+            'LineString': new ol.geom.LineString(coord),
+            'Polygon': new ol.geom.Polygon([coord])
+        }
+
+        var feature = new ol.Feature({
+            geometry: shape[geomType],
+            location: shape[geomType]	// for wfs-t
+        });
+
+        return feature;
+    }
+
+
+    var transformTo4326 = function(coord) {
+        var transCoord = [];
+        if(coord) {
+            transCoord = ol.proj.transform(coord, "EPSG:3857", "EPSG:4326");
+        }
+        return transCoord;
+    }
+
+    /*
+    draw.on('drawend', function(evt) {
+        var feature = evt.feature;
+        feature.set('geometry', feature.getGeometry());
+        var fid = feature.getId();
+        var node = format.writeTransaction([feature], null, null, {
+            gmlOptions: {srsName: "EPSG:3857"},
+            featureNS: "fiware",
+            featureType: "nyc_buildings"
+    });
+    */
+
     return {
     	create: function(element) {
     		map.setTarget(element);
@@ -81,110 +181,4 @@ var MapInit = function(policy) {
 //    return map.getView().getProjection();
 //},
 
-var formatWFS = new ol.format.WFS();
-var formatGML = new ol.format.GML({
-	featureNS: 'pokemap',
-	featureType: 'pokestop_info'
-	//srsName: 'EPSG:4326',
-	//version: '1.1.0'
-});
 
-var transactWFS = function(action, feature) {
-	if(feature == null)
-		return;
-
-
-	var node;
-	switch (action) {
-    case 'insert':
-    	feature.set('code', 'STOP');
-    	feature.set('name', 'Insert Test test test');
-    	node = formatWFS.writeTransaction([feature], null, null, formatGML);
-    	break;
-
-    case 'update':
-    	feature.set('code', 'STOP');
-    	feature.set('name', 'Update Update Update Update');
-    	node = formatWFS.writeTransaction(null, [feature], null, formatGML);
-    	break;
-
-    	case 'delete':
-    	node = formatWFS.writeTransaction(null, null, [feature], formatGML);
-    	break;
-	}
-
-	$.ajax({
-	    service: 'WFS',
-	    type: "POST",
-	    url: "http://localhost:8080/geoserver/pokemap/wfs",
-	    dataType: 'xml',
-	    processData: false,
-	    contentType: 'text/xml',
-	    data: new XMLSerializer().serializeToString(node),
-	    contentType: 'text/xml',
-	    success: function(data) {
-	    	debugger
-	    	console.log(formatWFS.readTransactionResponse(data));
-	    },
-	    error: function(e) {
-	    	console.error(e);
-	    },
-	    context: this
-//	}).done(function() {
-//	    vectorSource.clear();
-//	    map.removeInteraction(draw);
-//	    map.removeInteraction(select);
-//	    select.getFeatures().clear();
-	});
-};
-
-/**
- * 파라미터로 받은 좌표로 Feature를 생성하여 리턴한다.
- * @param   {string} geomType (Point, Polygon)
- * @param   {Array<string>} coord
- * @returns {Object} ol.Feature
- */
-var getFeatureByCoord = function(geomType, coord) {
-    // validation
-
-    var shape = {
-        'Point': new ol.geom.Point(coord),
-        'LineString': new ol.geom.LineString(coord),
-        'Polygon': new ol.geom.Polygon([coord])
-    }
-
-    var feature = new ol.Feature({
-        geometry: shape[geomType],
-        location: shape[geomType]	// for wfs-t
-    });
-
-    return feature;
-}
-
-
-var transformTo4326 = function(coord) {
-    var transCoord = [];
-    if(coord) {
-        transCoord = ol.proj.transform(coord, "EPSG:3857", "EPSG:4326");
-    }
-    return transCoord;
-}
-
-/*
-draw.on('drawend', function(evt) {
-    var feature = evt.feature;
-    feature.set('geometry', feature.getGeometry());
-    var fid = feature.getId();
-    var node = format.writeTransaction([feature], null, null, {
-        gmlOptions: {srsName: "EPSG:3857"},
-        featureNS: "fiware",
-        featureType: "nyc_buildings"
-});
-*/
-
-var Pokemap = Pokemap||{
-
-};
-
-Pokemap.GIS = new MapInit({});
-Pokemap.Map = Pokemap.GIS.create('map');
